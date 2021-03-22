609eca4e8c2161bc4c7e410fb5e5b356b50f18ca
JoanaSMatias <07JoanaSMatias@users.noreply.github.com>
2021-03-22 21:44:27 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
   return (
     <div className="App">
       <Container />
+      <footer>
+        This project was coded by Joana Matias and is open-sourced
+     <a
+       href="https://github.com/JoanaSMatias/react-weather-app"
+       target="_blank">
+         open-sourced on GitHub
+     </a>
+      </footer>
     </div>
   );
 }
