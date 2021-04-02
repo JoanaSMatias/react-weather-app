4f8259d863ccc2718fdece491994674f5b156a36
JoanaSMatias <07JoanaSMatias@users.noreply.github.com>
2021-04-02 22:57:56 +0100

    Added API LINK ICON

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ function handleResponse (response) {
     humidity: response.data.main.humidity,
     wind: response.data.wind.speed,
     city:response.data.name,
-    iconUrl: "",
+    iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].main}@2x.png`,
     iconDescription: response.data.weather[0].main,
     date: new Date (response.data.dt * 1000)
   });
