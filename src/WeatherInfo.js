64ce7527c15b0e8b70c7989fc78c91d3b54e558d
JoanaSMatias <07JoanaSMatias@users.noreply.github.com>
2021-04-07 20:39:21 +0100

    Correcting

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,30 +4,30 @@ import WeatherMetric from "./WeatherMetric"
 
 export default function WeatherInfo(props){
     return (
-        <div className = "WeatherInfo">
-<div className="container" id="cityDescriptionContainer">
-          <div className="row justify-content-md-center">
-            <div className="col-sx"></div>
+  <div className = "WeatherInfo">
+      <div className="container" id="cityDescriptionContainer">
+        <div className="row justify-content-md-center">
+          <div className="col-sx"></div>
+            <div className="col-md-auto">
+              <h2>{props.data.city}</h2>
+            </div>
+            <span id="descriptionBox">
               <div className="col-md-auto">
-                <h2>{props.data.city}</h2>
+                <WeatherIcon code={props.data.icon} />
               </div>
-              <span id="descriptionBox">
-                <div className="col-md-auto">
-                  <WeatherIcon code={props.data.icon} />
-                </div>
-                <WeatherMetric celsius = {props.data.temperature}/>
+              <WeatherMetric celsius = {props.data.temperature}/>
                 <div className="col" id="descriptionWeather">
-                    <span id="weatherResume">{props.data.description}</span>
+                  <span id="weatherResume">{props.data.description}</span>
                   <div className="col col-sx-2"></div>
                     <span id="humidity"></span>
                     <span id="percentageOfHumidity">Humidity: {props.data.humidity}%</span>
-                   <div className="col col-sx-2"></div>
+                  <div className="col col-sx-2"></div>
                     <span id="windSpeed"></span>
                     <span id="speed">Wind: {props.data.wind}Km/H</span>
                 </div>
-              </span>
-          </div>
-        </div>   
+            </span>
         </div>
+      </div>   
+    </div>
     )
 }
